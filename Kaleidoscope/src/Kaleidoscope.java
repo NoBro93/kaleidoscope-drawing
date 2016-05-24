5aa3c77c9eb45e5c8e2fe206aacf56654f8ee8ad
NoBro93 <ngb1311@yahoo.com>
2016-05-23 23:35:13 -0400

    added save option and a hide option for buttons

--- Kaleidoscope/src/Kaleidoscope.java
+++ Kaleidoscope/src/Kaleidoscope.java
@@ -8,35 +8,41 @@ public class Kaleidoscope extends PApplet {
   }
   
   private int windowX=500, windowY=500, but_height = windowY-60, but_size = 50, x2, y2, px2, py2;
-  private boolean clear=false;
-  
-//TODO: make clear an option object
+  private boolean goShow = true;
+
   Option X = new Option(this, "X2", 10, but_height, but_size, but_size);
   Option Y = new Option(this, "Y2", 70, but_height, but_size, but_size);
   Option F = new Option(this, "4", 130, but_height, but_size, but_size);
   Option E = new Option(this, "8", 190, but_height, but_size, but_size);
+  Option clear = new Option(this, "Clear", 10, 10, 50, 25);
+  Option save = new Option(this, "Save", windowX-60, 10, 50, 25);
+  Option hide = new Option(this, "Hide", windowX-60, windowY-30, 50, 25);
   
   public void setup() {
 	  size(windowX, windowY);
 	  background(0);
+	  showAll();
   }
   private boolean onScreen() {
-	  return (mouseX < windowX && mouseX > 0 && mouseY < windowY && mouseY > 0);
-  }
-  private boolean onClear(){
-	  return (mouseX > 10 && mouseX < 60 && mouseY > 10 && mouseY < 35);
+	  return (  mouseX < windowX && mouseX > 0 && mouseY < windowY && mouseY > 0 && 
+			  !( X.on() && goShow || Y.on() && goShow || F.on() && goShow || E.on() && goShow 
+					  || clear.on() && goShow || save.on() || hide.on()) );
   }
   public void draw() {
-    options();
-    if (clear){
+	if (goShow)
+		showAll();
+	save.show();
+	hide.show(); //Can't have whole screen if need unhide button...
+    if (clear.go){
     	background(0);
-    	clear = false;
+    	clear.go = false;
     }
-	if (mousePressed && onScreen()) { //TODO: stop it from picking up clicks on buttons
+	if (mousePressed && onScreen()) {
 	  	x2 = windowX - mouseX;
 	    px2 = windowX - pmouseX;
 	  	y2 = windowY - mouseY;
 	  	py2 = windowY - pmouseY;
+	  	
 	    if (X.go)
 	    	X2();
 	    if (Y.go)
@@ -48,6 +54,7 @@ public class Kaleidoscope extends PApplet {
 	}
   }
   public void mouseClicked() {
+	if (goShow){
 	  if (X.on()){
 		  X.go = true;
 		  Y.go = F.go = E.go = false;		  
@@ -64,55 +71,68 @@ public class Kaleidoscope extends PApplet {
 		  E.go = true;
 		  X.go = Y.go = F.go = false;
 	  }
-	  if (onClear()){
-		  clear = true;
+	  if (clear.on()){
+		  clear.go = true;
+	  }  
+	}
+	if (save.on()){
+		if (goShow)
+			hideAll();
+		save.hide();
+		hide.hide();
+	    saveFrame();
+	    save.show();
+	    hide.show();
+	}
+    if (hide.on()) {
+	  if (goShow){
+		  hideAll();
+	  	  goShow = false;
 	  }
+	  else {
+		  goShow = true;
+		  showAll();
+		  stroke(70, 35, 123);
+	  }  
+    }
   }
-  private void options(){
-	stroke(255);
-	X.show();    
-    Y.show();
-    F.show();
-    E.show();
-    
-    //clear
-    fill(0,0,0);
-    rect(10, 10, 50, 25);
-    textSize(15);
-    fill (255, 255, 255);
-    text("clear", 17, 27);
+  private void showAll(){
+	X.show();
+	Y.show();
+	F.show();
+	E.show();
+	clear.show();    
+  }
+  private void hideAll(){
+	X.hide();
+	Y.hide();
+	F.hide();
+	E.hide();
+	clear.hide();
   }
   private void X2(){
-	if (mousePressed && onScreen()) {
-	    line(mouseX,mouseY,pmouseX,pmouseY);
-	    line(x2,mouseY,px2,pmouseY);
-	}
+	line(mouseX,mouseY,pmouseX,pmouseY);
+	line(x2,mouseY,px2,pmouseY);
   }
   private void Y2(){
-	if (mousePressed && onScreen()) {
-	    line(mouseX,mouseY,pmouseX,pmouseY);
-	    line(mouseX,y2,pmouseX,py2);
-	}
+    line(mouseX,mouseY,pmouseX,pmouseY);
+    line(mouseX,y2,pmouseX,py2);
   }
   private void C4(){
-	if (mousePressed && onScreen()) {
-	    line(mouseX,mouseY,pmouseX,pmouseY);
-	    line(mouseX,y2,pmouseX,py2);
-	    line(x2,mouseY,px2,pmouseY);
-	    line(x2,y2,px2,py2);
-	}
+    line(mouseX,mouseY,pmouseX,pmouseY);
+    line(mouseX,y2,pmouseX,py2);
+    line(x2,mouseY,px2,pmouseY);
+    line(x2,y2,px2,py2);
   }
   private void C8(){
-	if (mousePressed && onScreen()) {
-	  	line(mouseX,mouseY,pmouseX,pmouseY);
-	    line(mouseX,y2,pmouseX,py2);
-	    line(x2,mouseY,px2,pmouseY);
-	    line(x2,y2,px2,py2);
-	    
-	    line(mouseY,mouseX,pmouseY,pmouseX);
-	    line(mouseY,x2,pmouseY,px2);
-	    line(y2,mouseX,py2,pmouseX);
-	    line(y2,x2,py2,px2);
-	}
+  	line(mouseX,mouseY,pmouseX,pmouseY);
+    line(mouseX,y2,pmouseX,py2);
+    line(x2,mouseY,px2,pmouseY);
+    line(x2,y2,px2,py2);
+    
+    line(mouseY,mouseX,pmouseY,pmouseX);
+    line(mouseY,x2,pmouseY,px2);
+    line(y2,mouseX,py2,pmouseX);
+    line(y2,x2,py2,px2);
   }
 }
--- Kaleidoscope/src/Option.java
+++ Kaleidoscope/src/Option.java
@@ -18,13 +18,27 @@ public class Option {
 		return (par.mouseX > minX && par.mouseX < maxX && par.mouseY > minY && par.mouseY < maxY);
 	}
 	public void show(){
+		int y;
+		par.stroke(255);
 	    par.fill(0,0,0);
 	    par.rect(minX, minY, maxX-minX, maxY-minY);
-	    par.textSize(25);
 	    par.fill(255,255,255);
-	    if (name.length()>1)
-	    	par.text(name, minX+10, minY+35);
+	    par.textSize(25);
+	    if (name.length()>1){
+	    	if (name.length()>2){
+	    		par.textSize(15);
+	    		y = minY+17;
+	    	}
+	    	else
+	    		y = minY+35;
+	    	par.text(name, minX+15-2*name.length(), y);
+	    }
 	    else
 	    	par.text(name,  minX+15, minY+35);
+	}
+	public void hide(){
+		par.fill(0,0,0);
+		par.stroke(0);
+		par.rect(minX, minY, maxX-minX, maxY-minY);
 	}
 }
